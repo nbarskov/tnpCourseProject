163fe64f1adf4572a699b7773dae1753048c4ff1
Barskov Nikita, Andrei Pavelev <nbarskov@gmail.com, 44dragon44@rambler.ru>
2017-05-09 17:11:40 +0400

    Lul, it works

--- src/main/resources/static/js/components/flights-table/flights-table.controller.js
+++ src/main/resources/static/js/components/flights-table/flights-table.controller.js
@@ -1,9 +1,17 @@
-angular.module("components.flights-table").controller("FlightsTableController", function () {
+angular.module("components.flights-table").controller("FlightsTableController", function (FlightsService) {
     var ctrl = this;
 
     ctrl.company = function (airCraft) {
         return ctrl.companies.find(function (c) {
 
         });
+    }
+
+    ctrl.buttonPressed = function(flight, tickets_bought, tickets_availible) {
+        var updateFlights;
+        tickets_availible = 300 - tickets_bought;
+        updateFlights = FlightsService.updateTickets(flight,tickets_bought,tickets_availible);
+        //updateFlights = FlightsService.updateTickets(20072, 4, 4);
+        updateFlights = FlightsService.all();
     }
 });
